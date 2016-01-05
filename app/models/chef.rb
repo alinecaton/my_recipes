8d766fccf83a5fc36903526ef4d90447d96a674c
Aline Caton <alinecaton@gmail.com>
2016-01-05 19:08:41 +1000

    creating chefs and recipes association

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,6 +1,8 @@
 class Chef < ActiveRecord::Base
   before_save { self.email = email.downcase }
 
+  has_many :recipes
+
   validates :chefname, presence: true, length: { minimum: 3, maximum: 40 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
