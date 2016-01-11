fad3004dc99b7fc8aab0be6ae03e9434267f7adb
Aline Caton <alinecaton@gmail.com>
2016-01-11 11:31:22 +1000

    creating show page for recipes and avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def gravatar_for(chef, option = { size: 300 })
+    gravatar_id = Digest::MD5::hexdigest(chef.email.downcase)
+    size = option[:size]
+    gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?d=#{size}"
+    image_tag(gravatar_url, alt: chef.chefname, class: "gravatar")
+  end
 end
