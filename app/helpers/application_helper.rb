3277af9115af154556eac23e03e778d94f611e9c
Aline Caton <alinecaton@gmail.com>
2016-01-17 15:21:20 +1000

    fixing gravatar size

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
-  def gravatar_for(chef, option = { size: 300 })
+  def gravatar_for(chef, option = { size: 100 })
     gravatar_id = Digest::MD5::hexdigest(chef.email.downcase)
     size = option[:size]
-    gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?d=#{size}"
+    gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: chef.chefname, class: "gravatar")
   end
 end
